d8a9b11ac55bcc959c405424fad70628aad28510
niuhuan <niuhuancn@outlook.com>
2022-01-03 18:09:15 +0800

    init version

--- lib/configs/configs.dart
+++ lib/configs/configs.dart
@@ -2,11 +2,13 @@ import 'package:jasmine/configs/reader_controller_type.dart';
 import 'package:jasmine/configs/reader_direction.dart';
 import 'package:jasmine/configs/reader_slider_position.dart';
 import 'package:jasmine/configs/reader_type.dart';
+import 'package:jasmine/configs/versions.dart';
 
 import 'pager_controller_mode.dart';
 import 'pager_view_mode.dart';
 
 Future initConfigs() async {
+  await initVersion();
   await initPagerControllerMode();
   await initPagerViewMode();
   await initReaderType();
