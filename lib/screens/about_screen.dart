a541d57b2602765129e120b85370ae57c4ab9061
niuhuan <niuhuancn@outlook.com>
2022-04-02 11:34:03 +0800

    Beautify about layout

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -5,6 +5,7 @@ import 'package:jasmine/screens/components/badge.dart';
 
 class AboutScreen extends StatefulWidget {
   const AboutScreen({Key? key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() {
     return _AboutState();
@@ -15,65 +16,124 @@ class _AboutState extends State<AboutScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("关于"),),
-        body: SafeArea(
-            child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(children: [
-                  _buildLogo(),
-                  _buildVersion(),
-                  Container(height: 30),
-                  _buildVersionText(),
-                ]))));
-  }
-
-  Widget _buildLogo(){
-    return Container(
-    width: 200,child: Image.asset(
-      "lib/assets/startup.webp",
-      fit: BoxFit.fitWidth,
-    ));
-  }
-
-  Widget _buildVersion() {
-    return LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      appBar: AppBar(
+        title: const Text("关于"),
+      ),
+      body: ListView(
         children: [
-          Text('软件版本:${currentVersion()}'),
-          Row(
-            children: [
-              Text(
-                  '检查更新:${latestVersion == null ? "没有检测到新版本" : "检测到新版本 : $latestVersion"}'),
-              TextButton(
-                  onPressed: () {
-                    manualCheckNewVersion(context);
-                  },
-                  child: const Text(
-                    "检查更新",
-                  ))
-            ],
-          ),
-          TextButton(
-              onPressed: () {
-                openUrl("https://github.com/niuhuan/jasmine/releases/");
-              },
-              child: const Text(
-                "点击这里去下载页面",
-              ))
+          const Divider(),
+          _buildLogo(),
+          const Divider(),
+          _buildCurrentVersion(),
+          const Divider(),
+          _buildNewestVersion(),
+          const Divider(),
+          _buildGotoGithub(),
+          const Divider(),
+          _buildVersionText(),
+          const Divider(),
         ],
-      );
-    });
+      ),
+    );
+  }
+
+  Widget _buildLogo() {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        double? width, height;
+        if (constraints.maxWidth < constraints.maxHeight) {
+          width = constraints.maxWidth / 2;
+        } else {
+          height = constraints.maxHeight / 2;
+        }
+        return Container(
+          padding: const EdgeInsets.all(10),
+          child: Center(
+            child: SizedBox(
+              width: width,
+              height: height,
+              child: Image.asset(
+                "lib/assets/startup.webp",
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildCurrentVersion() {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+      child: Text("当前版本 : ${currentVersion()}"),
+    );
+  }
+
+  Widget _buildNewestVersion() {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+      child: Text.rich(TextSpan(
+        children: [
+          const TextSpan(text: "最新版本 : "),
+          _buildNewestVersionSpan(),
+          _buildCheckButton(),
+        ],
+      )),
+    );
+  }
+
+  InlineSpan _buildNewestVersionSpan() {
+    return WidgetSpan(
+      child: Container(
+        padding: const EdgeInsets.only(right: 20),
+        child: VersionBadged(
+          child: Text(
+            "${latestVersion ?? "没有检测到新版本"}    ",
+          ),
+        ),
+      ),
+    );
+  }
+
+  InlineSpan _buildCheckButton() {
+    return WidgetSpan(
+      child: GestureDetector(
+        child: const Text(
+          "检查更新",
+          style: TextStyle(height: 1.3, color: Colors.blue),
+          strutStyle: StrutStyle(height: 1.3),
+        ),
+        onTap: () {
+          manualCheckNewVersion(context);
+        },
+      ),
+    );
+  }
+
+  Widget _buildGotoGithub() {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+      child: GestureDetector(
+        child: const Text(
+          "去下载地址",
+          style: TextStyle(color: Colors.blue),
+        ),
+        onTap: () {
+          openUrl("https://github.com/niuhuan/jasmine/releases/");
+        },
+      ),
+    );
   }
 
   Widget _buildVersionText() {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        return Center(
-          child: SelectableText(latestVersionInfo() ?? ""),
-        );
-      },
+    var info = latestVersionInfo();
+    if (info != null) {
+      info = "更新内容\n\n$info";
+    }
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: SelectableText(info ?? ""),
     );
   }
 }
