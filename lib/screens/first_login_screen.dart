d593d30362f87edac296f54c969924d7ab0e3e3d
niuhuan <niuhuancn@outlook.com>
2023-02-26 11:31:46 +0800

    :rocket: deploy

---
+++ lib/screens/first_login_screen.dart
@@ -0,0 +1,128 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import '../basic/commons.dart';
+import '../configs/versions.dart';
+import 'components/content_loading.dart';
+import '../configs/is_pro.dart';
+import '../configs/login.dart';
+import '../configs/network_api_host.dart';
+import '../configs/network_cdn_host.dart';
+import 'app_screen.dart';
+
+const firstLoginScreen = FirstLoginScreen();
+
+class FirstLoginScreen extends StatefulWidget {
+  const FirstLoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _FirstLoginScreenState();
+}
+
+class _FirstLoginScreenState extends State<FirstLoginScreen> {
+  bool _logging = false;
+  String _username = "";
+  String _password = "";
+  int _onClickVersion = 0;
+
+  Widget _usernameField() {
+    return ListTile(
+      title: const Text("账号"),
+      subtitle: Text(_username),
+      onTap: () async {
+        final input = await displayTextInputDialog(
+          context,
+          hint: "请输入账号",
+          title: "账号",
+          src: _username,
+        );
+        if (input != null) {
+          setState(() {
+            _username = input;
+          });
+        }
+      },
+    );
+  }
+
+  Widget _passwordField() {
+    return ListTile(
+      title: const Text("密码"),
+      subtitle: Text(_password.isEmpty ? "" : '********'),
+      onTap: () async {
+        final input = await displayTextInputDialog(
+          context,
+          hint: "请输入密码",
+          title: "密码",
+          isPasswd: true,
+          src: _password,
+        );
+        if (input != null) {
+          setState(() {
+            _password = input;
+          });
+        }
+      },
+    );
+  }
+
+  late final _saveButton = IconButton(
+    onPressed: () async {
+      setState(() {
+        _logging = true;
+      });
+      await login(_username, _password);
+      await reloadIsPro();
+      if (loginStatus != LoginStatus.loginSuccess) {
+        defaultToast(context, loginMessage);
+        setState(() {
+          _logging = false;
+        });
+      } else {
+        Navigator.pushReplacement(context, MaterialPageRoute(
+          builder: (BuildContext context) {
+            return const AppScreen();
+          },
+        ));
+      }
+    },
+    icon: const Icon(Icons.save),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("登录"),
+        actions: _logging
+            ? []
+            : [
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _onClickVersion++;
+                    });
+                    if (_onClickVersion >= 7) {
+                      openUrl(String.fromCharCodes(base64Decode("https://jmcomic1.rocks/signup")));
+                    }
+                  },
+                  icon: Text(currentVersion()),
+                ),
+                _saveButton,
+              ],
+      ),
+      body: ListView(
+        children: _logging
+            ? [
+                const Center(child: ContentLoading()),
+              ]
+            : [
+                _usernameField(),
+                _passwordField(),
+                apiHostSetting(),
+                cdnHostSetting(),
+              ],
+      ),
+    );
+  }
+}
