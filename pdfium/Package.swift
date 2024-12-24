e71b0fff1a4dc9a97160c86655da1fb5af5f3e53
niuhuan <niuhuancn@outlook.com>
2024-12-24 14:55:11 +0800

    :sparkles:

---
+++ pdfium/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "pdfium",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "pdfium",
+            type: .dynamic,
+            targets: ["pdfium"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "pdfium"),
+        .testTarget(
+            name: "pdfiumTests",
+            dependencies: ["pdfium"]
+        ),
+    ]
+)
---
+++ pdfium/Sources/pdfium/pdfium.swift
@@ -0,0 +1,7 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public func addTwo(to number: Int) -> Int {
+    return number + 2
+}
+
